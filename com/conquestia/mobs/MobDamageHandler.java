359b6b0d58b72c6868992fefeca12694515f0604
Ferrago <tkh0006@auburn.edu>
2014-09-25 21:37:12 -0500

    CQM FS & R
    Added comments and docs.

--- com/conquestia/mobs/MobDamageHandler.java
+++ com/conquestia/mobs/MobDamageHandler.java
@@ -1,12 +1,7 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.conquestia.mobs;
 
 import com.conquestia.mobs.Config.Config;
 import java.io.File;
-import java.util.ArrayList;
 import java.util.List;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -15,7 +10,6 @@ import org.bukkit.entity.Fireball;
 import org.bukkit.entity.Golem;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Monster;
-import org.bukkit.entity.Player;
 import org.bukkit.entity.Projectile;
 import org.bukkit.entity.SmallFireball;
 import org.bukkit.event.EventHandler;
@@ -24,51 +18,66 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
-import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
- *
+ * Handles mob damages. Adding modified damage values
+ * for higher level mobs.
+ * 
  * @author ferrago
  */
 public class MobDamageHandler implements Listener {
 
-    ConquestiaMobs cqm;
-    Config mobConfig;
-    double damageMultiplier;
-    ArrayList<Player> deadPlayers = new ArrayList();
+    ConquestiaMobs cqm; // Calling plugin
+    Config mobConfig; // Main config file
+    boolean dynamicFireDamage; //Modify fire damage?
 
+    
+    /**
+     * Initialize the config variable and cqm.
+     * Register the required listeners.
+     * 
+     * @param plugin Initializing plugin
+     */
     public MobDamageHandler(JavaPlugin plugin) {
         mobConfig = new Config(plugin, "Spawning" + File.separator + "MobSpawns");
         Bukkit.getServer().getPluginManager().registerEvents(this, plugin);
+        dynamicFireDamage = mobConfig.getConfig().getBoolean("DynamicFireDamage");
         cqm = (ConquestiaMobs) plugin;
     }
 
+    /**
+     * Retrieve the monsters level from it's name string.
+     * 
+     * @param entityName the name to parse for the level
+     * @return returns the monster's level
+     */
     private int getMobLevel(String entityName) {
         return Integer.parseInt(entityName.substring(entityName.indexOf(":") + 2, entityName.indexOf("]")));
     }
 
-    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
-    public void onPlayerDeath(PlayerDeathEvent event) {
-        deadPlayers.add(event.getEntity());
-
-        final Player deadPlayer = event.getEntity();
-        Bukkit.getScheduler().runTaskLater(cqm, new Runnable() {
-            public void run() {
-                deadPlayers.remove(deadPlayer);
-            }
-
-        }, 600);
-    }
-
+    /**
+     * Handles fire damage
+     * @param event 
+     */
     @EventHandler(priority = EventPriority.MONITOR)
     public void onFireDamage(EntityDamageEvent event) {
-        if (event.getEntity() instanceof LivingEntity && event.getCause() == DamageCause.FIRE_TICK) {
+        //If we don't want fire damage modifed then just exit.
+        if (!dynamicFireDamage) {
+            return;
+        } else if(event.getEntity() instanceof LivingEntity && event.getCause() == DamageCause.FIRE_TICK) {
 
             event.setDamage(((LivingEntity) event.getEntity()).getMaxHealth() * 0.02);
         }
     }
 
+    /**
+     * Handles mob damage, computes 
+     * level damage, as well as item in
+     * the mobs hand. (if applicable)
+     * 
+     * @param event the triggering event
+     */
     @EventHandler(priority = EventPriority.MONITOR)
     public void OnMobDamage(EntityDamageByEntityEvent event) {
         if (!mobConfig.getConfig().contains(event.getEntity().getWorld().getName() + ".DamageModifierEnabled") || !mobConfig.getConfig().getBoolean(event.getEntity().getWorld().getName() + ".DamageModifierEnabled")) {
@@ -88,7 +97,7 @@ public class MobDamageHandler implements Listener {
                     }
                 }
                 int level = getMobLevel(ChatColor.stripColor(le.getCustomName()));
-                damageMultiplier = mobConfig.getConfig().getDouble(event.getEntity().getWorld().getName() + ".DamageMultiplier", 0.1);
+                double damageMultiplier = mobConfig.getConfig().getDouble(event.getEntity().getWorld().getName() + ".DamageMultiplier", 0.1);
                 double newDamage = (event.getDamage() + (event.getDamage() * (level * damageMultiplier)));
                 newDamage += damageFromHand;
                 event.setDamage(newDamage);
@@ -108,7 +117,7 @@ public class MobDamageHandler implements Listener {
                     }
                 }
                 int level = getMobLevel(ChatColor.stripColor(le.getCustomName()));
-                damageMultiplier = mobConfig.getConfig().getDouble(event.getEntity().getWorld().getName() + ".DamageMultiplier", 0.1);
+                double damageMultiplier = mobConfig.getConfig().getDouble(event.getEntity().getWorld().getName() + ".DamageMultiplier", 0.1);
                 double newDamage = (event.getDamage() + (event.getDamage() * (level * damageMultiplier)));
 
                 int wave = 0;
@@ -131,7 +140,7 @@ public class MobDamageHandler implements Listener {
             } else {
                 level = getMobLevel(ChatColor.stripColor(((LivingEntity) ((Fireball) (event.getDamager())).getShooter()).getCustomName()));
             }
-            damageMultiplier = mobConfig.getConfig().getDouble(event.getEntity().getWorld().getName() + ".DamageMultiplier", 0.1);
+            double damageMultiplier = mobConfig.getConfig().getDouble(event.getEntity().getWorld().getName() + ".DamageMultiplier", 0.1);
             double newDamage = (event.getDamage() + (event.getDamage() * (level * damageMultiplier)));
 
             int wave = 0;
