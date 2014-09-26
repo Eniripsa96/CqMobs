a0782e09f05ff3c3b789cc793a8ad38b551a2380
Ferrago <tkh0006@auburn.edu>
2014-09-26 14:48:16 -0500

    FS & R
    Cleaned up code a little bit.
    Restructured data type to use UUID rather than player object.

--- com/conquestia/mobs/SkillAPIExperienceHandler.java
+++ com/conquestia/mobs/SkillAPIExperienceHandler.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package com.conquestia.mobs;
 
 import com.sucy.skill.api.event.PlayerExperienceGainEvent;
@@ -23,9 +17,11 @@ import org.bukkit.plugin.Plugin;
  * @author ferrago
  */
 public class SkillAPIExperienceHandler implements Listener {
-    HashMap<Player, LivingEntity> mobKillMap = new HashMap();
-    private double xpScale;
-    private Plugin plugin;
+    HashMap<String, LivingEntity> mobKillMap = new HashMap();
+    
+    private final double xpScale;
+    private final Plugin plugin;
+    
     public SkillAPIExperienceHandler(Plugin plugin, double experienceScale) {
         Bukkit.getServer().getPluginManager().registerEvents(this, plugin); 
         this.xpScale = experienceScale;
@@ -38,12 +34,12 @@ public class SkillAPIExperienceHandler implements Listener {
             return;
         }
         
-        if (mobKillMap.containsKey(event.getPlayerData().getPlayer())) {
-            LivingEntity ent = mobKillMap.get(event.getPlayerData().getPlayer());
+        if (mobKillMap.containsKey(event.getPlayerData().getPlayer().getUniqueId().toString())) {
+            LivingEntity ent = mobKillMap.get(event.getPlayerData().getPlayer().getUniqueId().toString());
             String entityName = ChatColor.stripColor(ent.getCustomName());
             int level = Integer.parseInt(entityName.substring(entityName.indexOf(":")+2, entityName.indexOf("]")));
             event.setExp((int)(event.getExp() + (level * xpScale)));
-            mobKillMap.remove(event.getPlayerData().getPlayer());
+            mobKillMap.remove(event.getPlayerData().getPlayer().getUniqueId().toString());
         }
         
     }
@@ -51,7 +47,7 @@ public class SkillAPIExperienceHandler implements Listener {
     @EventHandler(priority=EventPriority.LOWEST, ignoreCancelled=false)
     public void onEntityDeath(EntityDamageByEntityEvent event) {
         if (event.getDamager() instanceof Player && event.getEntity() instanceof LivingEntity && ((LivingEntity)event.getEntity()).getHealth() - event.getDamage() <= 0 && ((LivingEntity)event.getEntity()).getCustomName() != null && ((LivingEntity)event.getEntity()).getCustomName().contains("Lvl")) {
-            mobKillMap.put((Player)event.getDamager(), (LivingEntity)event.getEntity());
+            mobKillMap.put(((Player)event.getDamager()).getUniqueId().toString(), (LivingEntity)event.getEntity());
         }
     }
     
